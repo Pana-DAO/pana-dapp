df1a5b3f8da2b0f72274ae1af9c058e20b84e980
dasharatha232 <94668204+dasharatha232@users.noreply.github.com>
2022-10-10 11:25:22 +0530

    showing hours for days (#96)

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -189,14 +189,16 @@ export function prettifySeconds(seconds: number, resolution?: string) {
   const h = Math.floor((seconds % (3600 * 24)) / 3600);
   const m = Math.floor((seconds % 3600) / 60);
 
-  if (resolution === "day") {
-    return d + (d == 1 ? " day" : " days");
-  }
+  
 
   const dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
   const hDisplay = h > 0 ? h + (h == 1 ? " hr, " : " hrs, ") : "";
   const mDisplay = m > 0 ? m + (m == 1 ? " min" : " mins") : "";
 
+  if (resolution === "day") {
+    const dayResult = dDisplay + hDisplay;
+    return dayResult.slice(0, dayResult.length - 2);
+  }
   let result = dDisplay + hDisplay + mDisplay;
   if (mDisplay === "") {
     result = result.slice(0, result.length - 2);
